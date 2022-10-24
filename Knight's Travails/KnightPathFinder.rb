7466bd9fb05779e0d772818a5787f8ab96e2cc16
Andrea Cano <andreacanogisbert@gmail.com>
2022-10-24 14:49:43 -0700

    Finish KnightPathFinder

--- Knight's Travails/KnightPathFinder.rb
+++ Knight's Travails/KnightPathFinder.rb
@@ -2,58 +2,82 @@ require_relative "../skeleton/lib/00_tree_node.rb"
 
 class KnightPathFinder
 
-    def self.valid_moves(pos)
-        x, y = pos
-        valid = []
-       
-        (-2..2).each do |i|
-            if i != 0 && (x + i) >= 0 && (x + i) < 8
-                (-2..2).each do |j|
-                    if (y + j) >= 0 && (y + j) < 8 && j != 0 && j != i && i.abs != j.abs
+    attr_reader :start_pos, :root_node, :considered_positions
 
-                        valid << [x + i, y + j]
-                    end
-                end
+    MOVES = [
+        [-2, -1],
+        [-2,  1],
+        [-1, -2],
+        [-1,  2],
+        [ 1, -2],
+        [ 1,  2],
+        [ 2, -1],
+        [ 2,  1]
+      ]
+
+    def self.valid_moves(pos)
+        valid_moves = []
+        cur_x, cur_y = pos
+        MOVES.each do |(dx, dy)|
+            new_pos = [cur_x + dx, cur_y + dy]
+            if new_pos.all? { |coord| coord.between?(0, 7) }
+                valid_moves << new_pos
             end
         end
-
-        valid 
+        valid_moves
     end
 
     def initialize(start_pos)
-        @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
         @start_pos = start_pos
-        @tree = self.build_move_tree
+        @root_node = PolyTreeNode.new(start_pos)
+        build_move_tree
     end
 
     def new_move_positions(pos) 
         valid = KnightPathFinder.valid_moves(pos)
-         valid.each do |position|
-            if @considered_positions.include?(position)
-                valid.delete(position)
-            else
+        new_valid = []
+        valid.each do |position|
+            if !@considered_positions.include?(position)
                 @considered_positions << position
+                new_valid<<position
             end
         end
-
-        valid 
+        new_valid 
     end
 
-
     def build_move_tree
-        self.new_move_positions(pos)
-        nod_root = 00_tree_node.new(@start_pos)
-        new_moves = new_move_positions(@start_pos)
-        new_moves.each do |move|
-            node = 00_tree_node.new(move)
-            
+        queue = []
+        queue << @root_node
+        until queue.empty?
 
+            ele = queue.shift
+            new_moves = new_move_positions(ele.value)
+            new_moves.each do |move|
+                new_node = PolyTreeNode.new(move)
+                ele.add_child(new_node)
+                queue << new_node
+            end
+        end
+    end
+
+    def find_path(end_pos)
+        return @root_node.bfs(end_pos) 
+    end
+
+    def trace_path_back(end_pos)
+        arr = [end_pos]
+        last = find_path(end_pos)
+
+        current_par = last 
+        until current_par.nil?
+            current_par = current_par.parent
+            arr << current_par.value if current_par
+        end
+
+        arr.reverse 
     end
 
 
 end
 
-p KnightPathFinder.valid_moves([0, 0])
-p k = KnightPathFinder.new([0,0])
-p k.new_move_positions([0,0])
